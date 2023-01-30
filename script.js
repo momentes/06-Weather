9a0859c2138f4bfe8f2061be272303ffe3d12a5a
Bryan S <72003881+momentes@users.noreply.github.com>
2023-01-30 16:47:47 +1100

    Update HTML and Java

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 
-
-const cityAppend = document.querySelector("#citySearch");
-const cityInput = document.querySelector("#city");
-const citySearchInput = document.querySelector("#searchForCity");
-
-
-const previousSearchAppend = document.querySelector("#previousSearch");
-
-const weatherContent = document.querySelector("#weatherDashboard")
-
 const cities = [];
 
+const citySearch=document.getElementById("citySearch");
 
-let getCityWeather = function(city){
+const searchInput=document.getElementById("searchInput");
+const searchDisplay = document.getElementById("searchDisplay");
+const previousSearches = document.getElementById("previousSearches");
+
+const currentCastDashboard=document.getElementById("currentCastDashboard");
+
+const forecastHeader = document.getElementById("forecastHeader");
+
+
+var cities = [];
+
+
+var getSearchCityWeather = function(city){
     var apiKey = "523b55d0fba1e533fc90809576b9db58"
     var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
@@ -25,32 +28,60 @@ let getCityWeather = function(city){
 };
 
 
-let saveSearch = function(){
+var formSumbitHandler = function(event){
+    event.preventDefault();
+    var city = cityInput.value.trim();
+    if(city){
+        geSearchCityWeather(city);
+        cityInput.value = "";
+    } else{
+       placeholder("Please enter a City");
+    }
+    saveSearch();
+    pastSearch(city);
+}
+
+
+
+var saveSearch = function(){
     localStorage.setItem("cities", JSON.stringify(cities));
 };
 
 
 //Current Weather Dashboard
-const displayWeather = function(weather, searchForCity){
+var displayWeather = function(weather, previousCity){
 
-    // weatherContent.textContent = " "; 
-    // citySearchInput.textContent = searchForCity;
+    weatherContent.textContent = " "; 
+    cityPreviousSearch.textContent = previousCity;
 
     //create data 
-    let currentDate = document.createElement("span")
+    var currentDate = document.createElement("span")
     currentDate.textContent =" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
     citySearchInput.appendChild(currentDate);
  
     //create an weather icon
-    let weatherIcon = document.createElement("img")
+    var weatherIcon = document.createElement("img")
     weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weatherDaily.weather[0].icon}@2x.png`);
     citySearchInput.appendChild(weatherIcon);
  
-    //append card
-    let temperature = document.createElement("span");
+    //weather data with span element
+    var temperature = document.createElement("span");
     temperature.textContent = "Temperature: " + weather.main.temp + " °C";
-    temperature.classList = "listItem"
+    temperature.classList = "list"
 
-    
+    var wind = document.createElement("span");
+    wind.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+    wind.classList = "list"
+
+    var humidity = document.createElement("span");
+    humidity.textContent = "Humidity: " + weather.main.humidity + " %";
+    humidity.classList = "list"
+
+    weatherContent.appendChild(temperature);
+    weatherContent.appendChild(wind);
+    weatherContent.appendChild(humidity);
 
 }
+
+
+cityAppend.addEventListener("submit", formSumbitHandler);
