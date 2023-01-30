6f829bc8e68fd85e1e090219c5d79d698496ec64
Bryan S <72003881+momentes@users.noreply.github.com>
2023-01-30 21:38:04 +1100

    Updates

--- script.js
+++ script.js
@@ -10,17 +10,18 @@ const previousSearches = document.getElementById("previousSearches");
 const currentCastDashboard=document.getElementById("currentCastDashboard");
 
 const forecastHeader = document.getElementById("forecastHeader");
+const foreastList = document.getElementById("forecastList");
 
 const clearHistory = document.getElementById("clearHistory");
 
 
-// function toAllCaps(str){
-//     return str.toUpperCase();
-// }
+function toAllCaps(str){
+    return str.toUpperCase();
+}
 
-// function toTitleCase(str) {
-//     return str.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-// } 
+function toTitleCase(str) {
+    return str.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+} 
 
 
 const saveSearch = function(){
@@ -55,6 +56,29 @@ const formSumbitHandler = function(event){
         previousSearch(city, country);
     } else{
         searchInput.placeholder = "Please enter a city, country";
+    }
+}
+
+
+const previousSearch = function(previousSearchCity, previousSearchCountry){
+ 
+    previousSearchListElement = document.createElement("button");
+    previousSearchListElement.textContent = toTitleCase(previousSearchCity) + ' ' + toAllCaps(previousSearchCountry);
+    previousSearchListElement.classList = "d-flex w-100 btn-light border p-2";
+    previousSearchListElement.setAttribute("previousSearchCity",previousSearchCity)
+    previousSearchListElement.setAttribute("previousSearchCountry",previousSearchCountry)
+    previousSearchListElement.setAttribute("type", "submit");
+
+    previousSearches.prepend(previousSearchListElement);
+}
+
+
+
+var pastSearchHandler = function(event){
+    var city = event.target.getAttribute("previousSearchCity")
+    if(city){
+        getCurrentCast(city);
+        getForecast(city);
     }
 }
 
@@ -123,6 +147,70 @@ const getCurrentCast = function(city, country){
   
  }
  
+
+const getForecast = function(city, country){
+    let apiKey = "523b55d0fba1e533fc90809576b9db58"
+    let apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&units=metric&appid=${apiKey}`
+
+    fetch(apiURL)
+    .then(function(response){
+        response.json().then(function(data){
+           forecastDirectory(data);
+        });
+    });
+};
+
+
+const forecastDirectory = function(weather){
+    foreastList.textContent = ""
+    forecastHeader.textContent = "5 Day Forecast Weather:";
+
+    const forecast = weather.list;
+       for(var i=5; i < forecast.length; i=i+8){
+       const dailyForecast = forecast[i];
+        
+       
+       const forecastItem=document.createElement("div");
+       forecastItem.classList = "card bg-primary text-light m-2";
+
+       let forecastDate = document.createElement("h5")
+       forecastDate.textContent= moment.unix(dailyForecast.dt).format("MMM D, YYYY");
+       forecastDate.classList = "card-header text-center"
+
+       let weatherIcon = document.createElement("img")
+       weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${dailyForecast.weather[0].icon}@2x.png`);  
+       weatherIcon.classList = "card-body text-center";
+
+       let forecastDescription=document.createElement("span");
+       forecastDescription.textContent = toTitleCase(dailyForecast.weather[0].description);
+       forecastDescription.classList = "card-body text-center";
+
+       let forecastFeels=document.createElement("span");
+       forecastFeels.textContent = "Feels Like: " + dailyForecast.main.feels_like + " °C";
+       forecastFeels.classList = "card-body text-center";
+
+       let forecastTemperature=document.createElement("span");
+       forecastTemperature.textContent = "Temperature: " + dailyForecast.main.temp + " °C";
+       forecastTemperature.classList = "card-body text-center";
+
+       let forecastHumidity=document.createElement("span");
+       forecastHumidity.textContent = "Humidity: " + dailyForecast.main.humidity + "  %";
+       forecastHumidity.classList = "card-body text-center";
+
+
+       forecastItem.appendChild(forecastDate);
+       forecastItem.appendChild(weatherIcon);
+       forecastItem.appendChild(forecastDescription);
+       forecastItem.appendChild(forecastFeels);
+       forecastItem.appendChild(forecastTemperature);
+       forecastItem.appendChild(forecastHumidity);
+       
+
+        foreastList.appendChild(forecastItem);
+    }
+
+}
+
 
 citySearch.addEventListener("submit", formSumbitHandler);
 previousSearches.addEventListener("click", pastSearchHandler);
