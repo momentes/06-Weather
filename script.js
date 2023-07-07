412873c45dd9689c68226a46975dfad103cc131d
Bryan S <72003881+momentes@users.noreply.github.com>
2023-07-07 13:57:57 +1000

    Update Work

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 
+
 const cities = [];
 
 const citySearch=document.getElementById("citySearch");
-
 const searchInput=document.getElementById("searchInput");
-const searchDisplay = document.getElementById("searchDisplay");
-const previousSearches = document.getElementById("previousSearches");
-
 const currentCastDashboard=document.getElementById("currentCastDashboard");
-
+const searchDisplay = document.getElementById("searchDisplay");
 const forecastHeader = document.getElementById("forecastHeader");
 const foreastList = document.getElementById("forecastList");
+const previousSearches = document.getElementById("previousSearches");
+
 
 const clearHistory = document.getElementById("clearHistory");
 
@@ -21,21 +20,23 @@ function toAllCaps(str){
 
 function toTitleCase(str) {
     return str.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-} 
+  } 
 
 
-const saveSearch = function(){
-    localStorage.setItem("cities", JSON.stringify(cities));
-};
+const getCurrentCast = function(city, country){
+    let apiKey = "523b55d0fba1e533fc90809576b9db58"
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&appid=${apiKey}`
+ 
+     fetch(apiURL)
+     .then(function(response){
+         response.json().then(function(data){
+             currectCastDirectory(data, city);
+         });
+     });
+ };
 
-
-clearHistory.addEventListener("click", function(){
-    localStorage.removeItem("cities");
-    previousSearches.textContent = "";
-});
-
-
-const formSumbitHandler = function(event){
+ 
+ const formSumbitHandler = function(event){
     event.preventDefault();
     const cityCountry = searchInput.value.trim();
     if(cityCountry){
@@ -60,93 +61,68 @@ const formSumbitHandler = function(event){
 }
 
 
-const previousSearch = function(previousSearchCity, previousSearchCountry){
- 
-    previousSearchListElement = document.createElement("button");
-    previousSearchListElement.textContent = toTitleCase(previousSearchCity) + ' ' + toAllCaps(previousSearchCountry);
-    previousSearchListElement.classList = "d-flex w-100 btn-light border p-2";
-    previousSearchListElement.setAttribute("previousSearchCity",previousSearchCity)
-    previousSearchListElement.setAttribute("previousSearchCountry",previousSearchCountry)
-    previousSearchListElement.setAttribute("type", "submit");
+const saveSearch = function(){
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
 
-    previousSearches.prepend(previousSearchListElement);
-}
+
+clearHistory.addEventListener("click", function(){
+    localStorage.removeItem("cities");
+    previousSearches.textContent = "";
+  });
 
 
 
-var pastSearchHandler = function(event){
-    var city = event.target.getAttribute("previousSearchCity")
-    if(city){
-        getCurrentCast(city);
-        getForecast(city);
-    }
-}
-
-
-const getCurrentCast = function(city, country){
-    let apiKey = "523b55d0fba1e533fc90809576b9db58"
-    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&appid=${apiKey}`
- 
-     fetch(apiURL)
-     .then(function(response){
-         response.json().then(function(data){
-             currectCastDirectory(data, city);
-         });
-     });
- };
-
-
- const currectCastDirectory = function(weather, currentCity){
-    
-    //Clear Search
-    currentCastDashboard.textContent= "";  
-    searchDisplay.textContent = toTitleCase(currentCity);
-    
-    //Create Header Element 
-    let country = document.createElement("span")
-    country.textContent = ", " + weather.sys.country;
- 
-    let currentDate = document.createElement("span")
-    currentDate.textContent=" (" + moment(weather.dt.value).format("MMMM D, YYYY") + ") ";
- 
-    let weatherIcon = document.createElement("img")
-    weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
- 
-    //Create Description Element
-    let description = document.createElement("span");
-    description.textContent = "Description: " + toTitleCase(weather.weather[0].description);
-    description.classList = "list-group-item"
- 
-    let temperatureFeels = document.createElement("span");
-    temperatureFeels.textContent = "Feels Like: " + weather.main.feels_like + " °C";
-    temperatureFeels.classList = "list-group-item"
- 
-    let temperature = document.createElement("span");
-    temperature.textContent = "Temperature: " + weather.main.temp + " °C";
-    temperature.classList = "list-group-item"
+const currectCastDirectory = function(weather, currentCity){
+   //clear old content
+   currentCastDashboard.textContent= "";  
+   searchDisplay.textContent = toTitleCase(currentCity);
    
-    let humidity = document.createElement("span");
-    humidity.textContent = "Humidity: " + weather.main.humidity + " %";
-    humidity.classList = "list-group-item"
- 
-    let windSpeed = document.createElement("span");
-    windSpeed.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
-    windSpeed.classList = "list-group-item"
- 
-    //Append Header Element
-    searchDisplay.appendChild(country);
-    searchDisplay.appendChild(currentDate);
-    searchDisplay.appendChild(weatherIcon);
- 
-    //Append Description Element
-    currentCastDashboard.appendChild(description);
-    currentCastDashboard.appendChild(temperatureFeels);
-    currentCastDashboard.appendChild(temperature);
-    currentCastDashboard.appendChild(humidity);
-    currentCastDashboard.appendChild(windSpeed);
+      //create date element
+   let country = document.createElement("span")
+   country.textContent = ", " + weather.sys.country;
+
+   let currentDate = document.createElement("span")
+   currentDate.textContent=" (" + moment(weather.dt.value).format("MMMM D, YYYY") + ") ";
+
+   //create an image element
+   let weatherIcon = document.createElement("img")
+   weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+
+   let description = document.createElement("span");
+   description.textContent = "Description: " + toTitleCase(weather.weather[0].description);
+   description.classList = "list-group-item"
+
+   let temperatureFeels = document.createElement("span");
+   temperatureFeels.textContent = "Feels Like: " + weather.main.feels_like + " °C";
+   temperatureFeels.classList = "list-group-item"
+
+   let temperature = document.createElement("span");
+   temperature.textContent = "Temperature: " + weather.main.temp + " °C";
+   temperature.classList = "list-group-item"
   
- }
+   let humidity = document.createElement("span");
+   humidity.textContent = "Humidity: " + weather.main.humidity + " %";
+   humidity.classList = "list-group-item"
+
+   let windSpeed = document.createElement("span");
+   windSpeed.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+   windSpeed.classList = "list-group-item"
+
+
+   searchDisplay.appendChild(country);
+   searchDisplay.appendChild(currentDate);
+   searchDisplay.appendChild(weatherIcon);
+
+   //Append 
+   currentCastDashboard.appendChild(description);
+   currentCastDashboard.appendChild(temperatureFeels);
+   currentCastDashboard.appendChild(temperature);
+   currentCastDashboard.appendChild(humidity);
+   currentCastDashboard.appendChild(windSpeed);
  
+}
+
 
 const getForecast = function(city, country){
     let apiKey = "523b55d0fba1e533fc90809576b9db58"
@@ -173,10 +149,12 @@ const forecastDirectory = function(weather){
        const forecastItem=document.createElement("div");
        forecastItem.classList = "card bg-primary text-light m-2";
 
+       //create date element
        let forecastDate = document.createElement("h5")
        forecastDate.textContent= moment.unix(dailyForecast.dt).format("MMM D, YYYY");
        forecastDate.classList = "card-header text-center"
 
+           //create an image element
        let weatherIcon = document.createElement("img")
        weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${dailyForecast.weather[0].icon}@2x.png`);  
        weatherIcon.classList = "card-body text-center";
@@ -189,6 +167,7 @@ const forecastDirectory = function(weather){
        forecastFeels.textContent = "Feels Like: " + dailyForecast.main.feels_like + " °C";
        forecastFeels.classList = "card-body text-center";
 
+       //create temperature span
        let forecastTemperature=document.createElement("span");
        forecastTemperature.textContent = "Temperature: " + dailyForecast.main.temp + " °C";
        forecastTemperature.classList = "card-body text-center";
@@ -205,12 +184,37 @@ const forecastDirectory = function(weather){
        forecastItem.appendChild(forecastTemperature);
        forecastItem.appendChild(forecastHumidity);
        
-
+       //append to five day container
         foreastList.appendChild(forecastItem);
     }
 
 }
 
+
+const previousSearch = function(previousSearchCity, previousSearchCountry){
+ 
+    previousSearchListElement = document.createElement("button");
+    previousSearchListElement.textContent = toTitleCase(previousSearchCity) + ' ' + toAllCaps(previousSearchCountry);
+    previousSearchListElement.classList = "d-flex w-100 btn-light border p-2";
+    previousSearchListElement.setAttribute("previousSearchCity",previousSearchCity)
+    previousSearchListElement.setAttribute("previousSearchCountry",previousSearchCountry)
+    previousSearchListElement.setAttribute("type", "submit");
+
+    previousSearches.prepend(previousSearchListElement);
+}
+
+
+
+var pastSearchHandler = function(event){
+    var city = event.target.getAttribute("previousSearchCity")
+    if(city){
+        getCurrentCast(city);
+        getForecast(city);
+    }
+}
+
+
+// previousSearch();
 
 citySearch.addEventListener("submit", formSumbitHandler);
 previousSearches.addEventListener("click", pastSearchHandler);
